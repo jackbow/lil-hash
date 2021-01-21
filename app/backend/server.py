b3282fa881b6d85dc7914e72e4b99717a071004d
Jack Bowman <jackbow@umich.edu>
2021-01-21 00:55:01 -0800

    disable cors for dev

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,10 +1,14 @@
 from sanic import Sanic
 from sanic.response import json, redirect, empty
+from sanic_cors import CORS, cross_origin
+from urllib.parse import unquote
 from wordlist import words
 import random
 import redis
 
 app = Sanic("url-hash")
+if app.config.ENV == "dev":
+    cors = CORS(app, resources={r"/*": {"origins": "http://localhost:*"}})
 rdb = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/<key:string>")
@@ -17,15 +21,16 @@ async def unhashurl(request, key: str):
     else:
         return empty(status=404)
 
-@app.get("/hash/<duration_hours:int>/<url:string>")
-async def hashurl(request, duration_hours: int, url: str):
-    duration_hours = duration_hours if duration_hours < 24 else 24
-    duration_hours = duration_hours * 60 * 60
+@app.post("/hash")
+async def hashurl(request):
+    url = unquote(request.args.get("url"))
+    hours = int(request.args.get("hours"))
+    hours = hours if hours <= 24 else 24
     key = random.choice(words)
     i = 0
     while rdb.get(key) is not None:
         key = "".join([random.choice(words) for n in range(i)])
         i = i + 1
     rdb.set(key, url)
-    rdb.expire(key, duration_hours)
-    return json({"key": key, "duration_hours": duration_hours})
+    rdb.expire(key, hours * 60 * 60)
+    return json({"key": key, "hours": hours})
