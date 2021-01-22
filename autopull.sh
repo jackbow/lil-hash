4bf9a6197baa103a63d22dff0524c649a0afbdf2
Jack Bowman <jackbow@umich.edu>
2021-01-22 02:43:06 -0800

    command fixes

--- autopull.sh
+++ autopull.sh
@@ -5,12 +5,12 @@ if [[ $HASH != `git rev-parse HEAD` ]]
 then
   tmux kill-server
   tmux \
-    new-session  "cd frontend; npm i; npm build; read" \; \
-    split-window "pip install -r requirements.txt; sanic server.app read" \; \
+    new-session  "cd app/frontend; npm i; npm build; read" \; \
+    split-window "cd app; pip install -r requirements.txt; sanic server.app; read" \; \
     split-window "caddy run; read" \; \
     select-layout even-vertical
 fi
 
-# crontab
+# crontab -e
 # remember to chmod +x ./autopull.sh
 # 0 0 * * * ~/lil-hash/autopull.sh
