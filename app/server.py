a0b28e818ada9a83a48c50fd985f4115bf743814
Jack Bowman <jackbow@umich.edu>
2021-01-22 00:59:09 -0800

    move backend, sanic hosts frontend, update caddy

--- app/server.py
+++ app/server.py
@@ -1,18 +1,35 @@
 from sanic import Sanic
-from sanic.response import json, redirect, empty
+from sanic.response import json, redirect, empty, file
 from sanic_cors import CORS, cross_origin
+from sanic_brotli import Compress
 from urllib.parse import unquote
 from wordlist import words
 import random
 import redis
+from os import listdir
 
 app = Sanic("lil' hash")
-if hasattr(app.config, 'ENV') and app.config.ENV == "dev":
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['COMPRESS_LEVEL'] = 11
+app.config['COMPRESS_MIN_SIZE'] = 400
+Compress(app)
+
 rdb = redis.Redis(host='localhost', port=6379, db=0)
 
+if hasattr(app.config, 'ENV') and app.config.ENV == "dev":
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+# static routing
+app.static("/", "./frontend/dist/index.html")
+for file_name in listdir("frontend/dist"):
+    file_ext = file_name.split(".").pop()
+    if file_ext == "svg" or file_ext == "png" or file_ext == "ico":
+        app.static("/" + file_name, "frontend/dist/" + file_name)
+for file_name in listdir("frontend/dist/_assets"):
+    app.static("/_assets/" + file_name, "frontend/dist/_assets/" + file_name)
+
+
 @app.get("/<key:string>")
-async def unhashurl(request, key: str):
+async def unhash_url(request, key: str):
     if rdb.get(key) is not None:
         url = rdb.get(key).decode()
         if url.find("://") == -1:
@@ -22,7 +39,7 @@ async def unhashurl(request, key: str):
         return empty(status=404)
 
 @app.post("/hash")
-async def hashurl(request):
+async def hash_url(request):
     url = unquote(request.args.get("url"))
     hours = int(request.args.get("hours"))
     hours = hours if hours <= 24 else 24
