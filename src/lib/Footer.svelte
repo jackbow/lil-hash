016d2245ce8071575a61876d222d01f067d457c4
Jack Bowman <jackbow@umich.edu>
2021-10-08 14:40:48 -0600

    add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,19 @@
+<footer class="width-screen mt-2">
+	<div class="margin-auto flex justify-center space-x-4">
+		<a href="https://jackbow.pages.dev/" target="_blank" class="hover:text-white transition duration-500">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<circle cx="12" cy="7" r="4" />
+				<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+			</svg>
+		</a>
+		<a href="https://github.com/jackbow/lil-hash" target="_blank" class="hover:text-white transition duration-500">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<polyline points="7 8 3 12 7 16" />
+				<polyline points="17 8 21 12 17 16" />
+				<line x1="14" y1="4" x2="10" y2="20" />
+			</svg>
+		</a>
+	</div>
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Footer from '$lib/Footer.svelte'
 	import { goto } from '$app/navigation';
 	import isValidUrl from '$lib/valid-url.js';
 	let inputUrl = '';
@@ -25,34 +26,30 @@
 	};
 </script>
 
-<h1 class="text-off-black text-7xl text-black lg:text-8xl font-averia mt-40 dark:text-white">
-	lil' hash
-</h1>
-<p class="text-off-black text-xl text-black lg:text-2xl font-averia dark:text-white">
-	simple sharable url shortener
-</p>
-<label for="url" class="hidden"> URL to shorten </label>
-<input
-	id="url"
-	bind:value={inputUrl}
-	class="shadow-inner rounded-md text-md w-3/5 md:w-1/2 lg:text-2xl p-2 md:p-3 lg:p-4 m-4 lg:my-10 text-center"
-	on:keyup={(e) => e.key === 'Enter' && hash()}
-/>
-<button
-	class="px-3 md:px-5 lg:px-8 py-2 md:py-3 lg:py-4 text-md lg:text-2xl rounded-md transition duration-500 bg-white shadow"
-	class:bg-red-500={errorText}
-	on:click={hash}
-	disabled
->
-	{errorText ? errorText : 'hash'}
-</button>
+<main>
+	<h1 class="mt-40 text-off-black text-7xl text-black lg:text-8xl font-averia dark:text-white">
+		lil' hash
+	</h1>
+	<p class="text-off-black text-xl text-black lg:text-2xl font-averia dark:text-white">
+		simple sharable url shortener
+	</p>
+	<label for="url" class="hidden"> URL to shorten </label>
+	<input
+		id="url"
+		bind:value={inputUrl}
+		class="shadow-inner rounded-md text-md w-3/5 md:w-1/2 lg:text-2xl p-2 md:p-3 lg:p-4 m-4 lg:my-10 text-center"
+		on:keyup={(e) => e.key === 'Enter' && hash()}
+	/>
+	<button
+		class="px-3 md:px-5 lg:px-8 py-2 md:py-3 lg:py-4 text-md lg:text-2xl rounded-md transition duration-500 bg-white shadow"
+		class:bg-red-500={errorText}
+		on:click={hash}
+		disabled
+	>
+		{errorText ? errorText : 'hash'}
+	</button>
+</main>
+<Footer />
 
 <style>
-	.text-off-black {
-		color: #35332c;
-	}
-	.font-averia {
-		font-family: 'Averia Serif';
-		font-weight: 300;
-	}
 </style>
